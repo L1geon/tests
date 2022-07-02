e75828e3efe84f18f1ee3158aa1552af09fb9b9f
L1geon <ligeon1@piterov.ru>
2022-07-02 19:29:44 +0300

    add API

---
+++ ya_disk_api.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            "Content-Type": "application/json",
+            "Authorization": f"OAuth {self.token}"
+        }
+
+    def creating_a_folder(self, file_path: str):
+        url = "https://cloud-api.yandex.net/v1/disk/resources"
+        headers = self.get_headers()
+        params = {"path": file_path}
+        response = requests.put(url, params=params, headers=headers)
+        print(response.json())
+        return response.status_code
+
